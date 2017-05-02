400e2d6fe4f021d0bca833add6c46fa1d6b921cf
Paul Williams <kwakwaversal@gmail.com>
2017-05-02 12:49:24 +0100

    Add dotfiles management aliases
    Using henrik's aliases to add the ability to reload, pull and edit
    dotfiles. This forms the `management` aliases.
    
    Also added a few git aliases to improve git productivity.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,3 +1,8 @@
+# management
+alias dots="cd ~/.dotfiles && vim"
+alias reload='source ~/.bash_profile && echo "sourced ~/.bash_profile"'
+alias redot='cd ~/.dotfiles && git pull && ./install.sh; cd -'
+
 # docker related
 alias d='docker'
 alias d-ips='docker ps -q | while read cid b; do echo -n "$cid "; d inspect $cid | jq -r ".[0].NetworkSettings.Networks | to_entries[] | .value.IPAddress"; done'
@@ -5,3 +10,11 @@ alias d-ips='docker ps -q | while read cid b; do echo -n "$cid "; d inspect $cid
 # docker-compose related (don't care about the dc precision calculator)
 alias dc='docker-compose'
 
+# git
+alias g="git"
+alias gs="git status"
+alias gw="git show"
+alias gw^="git show HEAD^"
+alias gw^^="git show HEAD^^"
+alias gw^^^="git show HEAD^^^"
+alias gw^^^^="git show HEAD^^^^"
