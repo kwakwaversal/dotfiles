19c27eff7b58bf8aeaff5734e6e442c650b17350
Paul Williams <kwakwaversal@gmail.com>
2017-05-02 13:59:17 +0100

    Add tmux shortcuts and theme

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 PROFILE="$HOME/.bash_profile"
+TMUX="$HOME/.tmux.conf"
 
 if [ -e "$PROFILE" ]
 then
    echo "File $PROFILE exists. Doing nothing."
 else
    ln -s ~/.dotfiles/bash_profile $PROFILE
+fi
+
+if [ -e "$TMUX" ]
+then
+   echo "File $TMUX exists. Doing nothing."
+else
+   ln -s ~/.dotfiles/tmux.conf $TMUX
 fi
 
 git config --global include.path .dotfiles/.gitconfig
