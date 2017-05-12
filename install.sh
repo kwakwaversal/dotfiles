2d7dc3a2392776780b6716768d36732de48236db
Paul Williams <kwakwaversal@gmail.com>
2017-05-12 09:22:34 +0100

    Add .psqlrc
    A better prompt and defaults have been added to the psqlrc file.
    
    http://www.craigkerstiens.com/2013/02/21/more-out-of-psql/

--- install.sh
+++ install.sh
@@ -1,40 +1,31 @@
 #!/usr/bin/env bash
 
-PROFILE="$HOME/.bash_profile"
-TMUX="$HOME/.tmux.conf"
+declare -A dotfiles
+dotfiles["bash_profile"]="$HOME/.bash_profile"
+dotfiles["tmux.conf"]="$HOME/.tmux.conf"
+dotfiles["psqlrc"]="$HOME/.psqlrc"
 
 echo "Bootstrapping environment to use ~/.dotfiles"
 
-echo ""
-echo "Symlink $PROFILE:"
-echo "  - ln -s ~/.dotfiles/bash_profile $PROFILE"
-if [ -e "$PROFILE" ]
-then
-   echo "    - Already exists. Doing nothing."
-else
-   ln -s ~/.dotfiles/bash_profile $PROFILE
-   if [ -e "$PROFILE" ]
-   then
-       echo "    + Added"
-   else
-       echo "    - Could not create symlink?"
-   fi
-fi
+for file in ${!dotfiles[@]}; do
+    FILE=${file}
+    LINK=${dotfiles[${file}]}
 
-echo ""
-echo "Symlink $TMUX:"
-echo "  - ln -s ~/.dotfiles/tmux.conf $TMUX"
-if [ -e "$TMUX" ]
-then
-   echo "    - Already exists. Doing nothing."
-else
-   ln -s ~/.dotfiles/tmux.conf $TMUX
-   if [ -e "$TMUX" ]
-   then
-       echo "    + Added"
-   else
-       echo "    - Could not create symlink?"
-   fi
-fi
+    echo ""
+    echo "Symlink $LINK:"
+    echo "  - ln -s ~/.dotfiles/$FILE $LINK"
+    if [ -e "$LINK" ]
+    then
+        echo "    - Already exists. Doing nothing."
+    else
+        ln -s ~/.dotfiles/$FILE $LINK
+        if [ -e "$LINK" ]
+        then
+            echo "    + Added"
+        else
+            echo "    - Could not create symlink?"
+        fi
+    fi
+done
 
 git config --global include.path .dotfiles/.gitconfig
