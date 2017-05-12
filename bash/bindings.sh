35b856c855155d83c5d99325e39c50db559cd787
Paul Williams <kwakwaversal@gmail.com>
2017-05-12 08:17:50 +0100

    Relay ALT+l as CTRL+l instead of execute :clear
    Originally I mapped the key sequence ALT+L to execute the command
    `clear` but found that it made more sense just to relay the key as the
    one it was replacing.

--- bash/bindings.sh
+++ bash/bindings.sh
@@ -1,13 +1,17 @@
 # Helpful bind-related commands
 #
 # `bind -p`     list *all* the current bindings
-# `bind -X`     for user-specific bindings
+# `bind -X`     list key sequences bound with -x
 #
 # http://www.skorks.com/2009/09/bash-shortcuts-for-maximum-productivity/
 # http://www.lbreyer.com/bash-smart-parsing-howto.html
 
 # CTRL+L has been overridden for the tmux pane movement so using ALT+L instead.
-bind -x '"\el"':clear
+#
+# N.B., This has been changed from using the shell command, to just forwarding
+# a new key sequence on as CTRL+l.
+# bind -x '"\el"':clear
+bind '"\el": "\C-l"'
 
 # Overriding bash shortcut CTRL+f (move forward 1 character) to append `| fzf`
 # to the end of the current command if `fzf` exists in the path.
