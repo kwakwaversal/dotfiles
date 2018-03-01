e38b1171a9ffadfc0e818e775df68660e66f14a1
Paul Williams <kwakwaversal@gmail.com>
2018-03-01 10:18:04 +0000

    Add alias dpwd (docker with volume-mounted pwd)
    The new shell alias `dpwd` provides the common flags to start an
    ephemeral interactive docker container. It also adds a volume of
    the current `pwd` as `/pwd` in the container.
    
    $ dpwd hello-world
    $ dpwd perl
    $ dpwd --entrypoint bash perl

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -6,6 +6,7 @@ alias redot='cd ~/.dotfiles && git pull && ./install.sh; cd -'
 # docker related
 alias d='docker'
 alias d-ips='docker ps -q | while read cid b; do echo -n "$cid "; d inspect $cid | jq -r ".[0].NetworkSettings.Networks | to_entries[] | .value.IPAddress"; done'
+alias dpwd='docker run -it --rm -v $(pwd):/pwd'
 
 # docker-compose related (don't care about the dc precision calculator)
 alias dc='docker-compose'
