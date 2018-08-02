5c3a6c268f867bb33a41bbdec1391f6058e91071
Paul Williams <kwakwaversal@gmail.com>
2018-08-02 22:50:41 +0000

    Add myinstall.sh
    Added new script which is intended to provision a fresh system with
    things I need. e.g., my vimfiles, fzf etc.

---
+++ myinstall.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This is for me and me alone. Sets up a fresh environment with things I need
+
+echo ""
+echo "Setting up environment:"
+
+if [ -e "$HOME/.bash_profile" ]
+then
+    echo "  - mv ~/.bash_profile ~/.profile.local"    
+    mv "$HOME/.bash_profile" "$HOME/.profile.local"
+fi
+
+if [ ! -e "$HOME/.vim" ]
+then
+    echo "  - git clone vimfiles"
+    git clone --recursive https://github.com/kwakwaversal/vimfiles $HOME/.vim
+fi
+
+if [ ! -e "$HOME/.fzf" ]
+then
+    echo "  - git clone fzf"
+    git clone https://github.com/junegunn/fzf.git $HOME/.fzf
+    echo "  - install fzf"
+    $HOME/.fzf/install
+fi
+
+$HOME/.dotfiles/install.sh
