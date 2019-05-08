f8548839d0297645196fab852d6ef171c184d5ee
Paul Williams <kwakwaversal@gmail.com>
2019-05-08 21:17:10 +0000

    Add my.cnf with custom MySQL prompt
    This also includes a better pager option with `less` that also supports
    very wide tables.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 declare -A dotfiles
 dotfiles["bash_profile"]="$HOME/.bash_profile"
+dotfiles["my.cnf"]="$HOME/.my.cnf"
 dotfiles["psqlrc"]="$HOME/.psqlrc"
 dotfiles["perltidyrc"]="$HOME/.perltidyrc"
 dotfiles["tmux.conf"]="$HOME/.tmux.conf"
